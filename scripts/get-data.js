1eacea29b001445ebc1df29c5c6952f0ed40f5b6
Joey Pinhas <jpinhas97@gmail.com>
2019-03-01 16:38:20 -0500

    Get the data retrieval working

--- scripts/get-data.js
+++ scripts/get-data.js
@@ -10,61 +10,99 @@
     function onReady(smart)  {
       if (smart.hasOwnProperty('patient')) {
         var patient = smart.patient;
+        // get a promise object of the patient's demographic data
         var pt = patient.read();
-        var obv = smart.patient.api.fetchAll({
-                    type: 'Observation',
-                    query: {
-                      code: {
-                        $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
-                              'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
-                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
-                      }
-                    }
-                  });
+        // fetch all conditions for the current patient that match our criteria
+        // smart-Condition-393 === http://snomed.info/sct/195967001
+        var conds = smart.patient.api.fetchAll({
+          type: 'Condition',
+          query: {
+            _id: {
+              $or: ['smart-Condition-393', 'smart-Condition-344', 'http://snomed.info/sct/195967001']
+            }
+          }
+        });
 
-        $.when(pt, obv).fail(onError);
+        // register error callbacks on the fetches
+        $.when(pt, conds).fail(onError);
 
-        $.when(pt, obv).done(function(patient, obv) {
-          var byCodes = smart.byCodes(obv, 'code');
-          var gender = patient.gender;
+        // register success callbacks on the fetches
+        $.when(pt, conds).done(function(patient, conditions) {
+          console.log(patient)
+          console.log(conditions)
+          var medicationsInfo = window.medicationsInfo;
 
-          var fname = '';
-          var lname = '';
-
-          if (typeof patient.name[0] !== 'undefined') {
-            fname = patient.name[0].given.join(' ');
-            lname = patient.name[0].family.join(' ');
+          var demoData = getDemoData(patient);
+          var asthmaConditions = getAsthmaConditions(conditions);
+          var recs = [];
+          var age = demoData.age;
+          if (asthmaConditions.length > 0) {
+            console.log(medicationsInfo)
+            for (var i = 0; i < medicationsInfo.length; i++) {
+              var current = medicationsInfo[i];
+              console.log(age, current)
+              if (age >= current.ageMin && age <= current.ageMax) {
+                recs.push(current);
+                console.log(true, current)
+              }
+            }
+          } else {
+            recs = 'none';
           }
 
-          var height = byCodes('8302-2');
-          var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
-          var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
-          var hdl = byCodes('2085-9');
-          var ldl = byCodes('2089-1');
+          var response = {
+            patient: demoData,
+            reccomendations: recs
+          };
 
-          var p = defaultPatient();
-          p.birthdate = patient.birthDate;
-          p.gender = gender;
-          p.fname = fname;
-          p.lname = lname;
-          p.height = getQuantityValueAndUnit(height[0]);
-
-          if (typeof systolicbp != 'undefined')  {
-            p.systolicbp = systolicbp;
-          }
-
-          if (typeof diastolicbp != 'undefined') {
-            p.diastolicbp = diastolicbp;
-          }
-
-          p.hdl = getQuantityValueAndUnit(hdl[0]);
-          p.ldl = getQuantityValueAndUnit(ldl[0]);
-
-          ret.resolve(p);
+          ret.resolve(response);
         });
       } else {
         onError();
       }
+    }
+
+    function getDemoData(patient) {
+      // get basic data from the patient
+      var gender = patient.gender;
+      var fname = '';
+      var lname = '';
+
+      // merge together multiple first names into one field
+      if (typeof patient.name[0] !== 'undefined') {
+        fname = patient.name[0].given.join(' ');
+        lname = patient.name[0].family.join(' ');
+      }
+
+      var birthdate = patient.birthDate;
+      var ageInYears = moment().diff(birthdate, 'years');
+      var ageInDays = moment().diff(birthdate, 'days');
+
+      var demoData = {
+        fname: fname,
+        lname: lname,
+        gender: gender,
+        birthdate: birthdate,
+        age: ageInDays / 365
+      };
+
+      return demoData;
+    }
+
+    function getAsthmaConditions(conditions) {
+      var asthmaConditionsCodes = ['smart-Condition-393', 'smart-Condition-344'];
+      var patientAsthmaCodes = []
+      for (var i = 0; i < conditions.length; i++) {
+        console.log(conditions[i])
+        var current = conditions[i]
+        for (var j = 0; j < asthmaConditionsCodes.length; j++) {
+          if (current.id === asthmaConditionsCodes[j]) {
+            patientAsthmaCodes.push(asthmaConditionsCodes[j]);
+          }
+        }
+      }
+      console.log(patientAsthmaCodes)
+      return patientAsthmaCodes;
     }
 
     FHIR.oauth2.ready(onReady, onError);
@@ -72,60 +110,17 @@
 
   };
 
-  function defaultPatient(){
-    return {
-      fname: {value: ''},
-      lname: {value: ''},
-      gender: {value: ''},
-      birthdate: {value: ''},
-      height: {value: ''},
-      systolicbp: {value: ''},
-      diastolicbp: {value: ''},
-      ldl: {value: ''},
-      hdl: {value: ''},
-    };
-  }
-
-  function getBloodPressureValue(BPObservations, typeOfPressure) {
-    var formattedBPObservations = [];
-    BPObservations.forEach(function(observation){
-      var BP = observation.component.find(function(component){
-        return component.code.coding.find(function(coding) {
-          return coding.code == typeOfPressure;
-        });
-      });
-      if (BP) {
-        observation.valueQuantity = BP.valueQuantity;
-        formattedBPObservations.push(observation);
-      }
-    });
-
-    return getQuantityValueAndUnit(formattedBPObservations[0]);
-  }
-
-  function getQuantityValueAndUnit(ob) {
-    if (typeof ob != 'undefined' &&
-        typeof ob.valueQuantity != 'undefined' &&
-        typeof ob.valueQuantity.value != 'undefined' &&
-        typeof ob.valueQuantity.unit != 'undefined') {
-          return ob.valueQuantity.value + ' ' + ob.valueQuantity.unit;
-    } else {
-      return undefined;
-    }
-  }
-
-  window.drawVisualization = function(p) {
+  window.displayPatientData = function(p) {
     $('#holder').show();
     $('#loading').hide();
     $('#fname').html(p.fname);
     $('#lname').html(p.lname);
     $('#gender').html(p.gender);
     $('#birthdate').html(p.birthdate);
-    $('#height').html(p.height);
-    $('#systolicbp').html(p.systolicbp);
-    $('#diastolicbp').html(p.diastolicbp);
-    $('#ldl').html(p.ldl);
-    $('#hdl').html(p.hdl);
+  };
+
+  window.displayReccomendations = function(r) {
+    console.log(r)
   };
 
 })(window);
